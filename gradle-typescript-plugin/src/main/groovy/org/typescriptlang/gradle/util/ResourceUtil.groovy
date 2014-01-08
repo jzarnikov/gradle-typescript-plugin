762162c0c1793e0caf4649480b3f46b569e806a3
Jan Zarnikov <jan.zarnikov@irian.at>
2014-01-08 12:16:22 +0100

    fixed copying of test libraries

--- gradle-typescript-plugin/src/main/groovy/org/typescriptlang/gradle/util/ResourceUtil.groovy
+++ gradle-typescript-plugin/src/main/groovy/org/typescriptlang/gradle/util/ResourceUtil.groovy
@@ -5,6 +5,9 @@ class ResourceUtil {
     static void copyResource(File destinationDir, String resourcePath) {
         String fileName = resourcePath.substring(resourcePath.lastIndexOf("/"))
         File destinationFile = new File(destinationDir, fileName)
+        if (destinationFile.exists()) {
+            destinationFile.delete()
+        }
         InputStream resourceAsStream = ResourceUtil.class.getResourceAsStream(resourcePath)
         destinationFile << resourceAsStream.text
     }
