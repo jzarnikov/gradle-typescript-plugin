51817b293c8d555e62e0297964d8c705ca527d44
Jan Zarnikov <jan.zarnikov@irian.at>
2014-01-14 13:53:05 +0100

    task descriptions

--- gradle-typescript-plugin/src/main/groovy/at/irian/typescript/gradle/TypeScriptPlugin.groovy
+++ gradle-typescript-plugin/src/main/groovy/at/irian/typescript/gradle/TypeScriptPlugin.groovy
@@ -26,38 +26,39 @@ class TypeScriptPlugin implements Plugin<Project> {
     void apply(Project target) {
         target.extensions.create("typescript", TypeScriptPluginExtension, target);
 
-        this.createTask("compileTypeScript", CompileMainTask, target)
-        this.createTask("compileTestTypeScript", CompileTestTask, target)
+        this.createTask("compileTypeScript", "Compile TypeScript source code", CompileMainTask, target)
+        this.createTask("compileTestTypeScript", "Compile TypeScript test source code", CompileTestTask, target)
 
 
-        this.createTask("initTestResources", InitTestResourcesTask, target)
-        this.createTask("prepareTestJs", PrepareTestLibsTask, target)
-        this.createTask("generateTestHtml", GenerateTestHtmlTask, target)
+        this.createTask("initTestResources", "Initialize test resources (currently just jasmine.d.ts)", InitTestResourcesTask, target)
+        this.createTask("prepareTestLibs", "Prepare libraries requried to run the tests", PrepareTestLibsTask, target)
+        this.createTask("generateTestHtml", "Generate HTML required to run the tests", GenerateTestHtmlTask, target)
 
-        this.createTask("test", RunTestInConsoleTask, target).dependsOn "generateTestHtml", "prepareTestJs", "compileTestTypeScript"
+        this.createTask("test", "Run the tests in console mode", RunTestInConsoleTask, target).dependsOn "generateTestHtml", "prepareTestLibs", "compileTestTypeScript"
 
-        this.createTask("testInBrowser", RunTestInBrowserTask, target).dependsOn "generateTestHtml", "prepareTestJs", "compileTestTypeScript"
+        this.createTask("testInBrowser", "Run the tests in browser", RunTestInBrowserTask, target).dependsOn "generateTestHtml", "prepareTestLibs", "compileTestTypeScript"
 
-        this.createTask("cleanMain", CleanMainTask, target)
+        this.createTask("cleanMain", "Delete compiled JavaScript", CleanMainTask, target)
 
-        this.createTask("cleanTestHtml", CleanTestHtmlTask, target)
-        this.createTask("cleanTestJs", CleanTestJsTask, target)
-        this.createTask("cleanTestLibs", CleanTestLibsTask, target)
+        this.createTask("cleanTestHtml", "Delete generated HTML required to run the tests", CleanTestHtmlTask, target)
+        this.createTask("cleanTestJs", "Delete compile test JavaScript", CleanTestJsTask, target)
+        this.createTask("cleanTestLibs", "Delete libraries requried to run the tests", CleanTestLibsTask, target)
 
-        this.createTask("cleanTest", DefaultTask, target).dependsOn "cleanTestHtml", "cleanTestJs", "cleanTestLibs"
+        this.createTask("cleanTest", "Delete all generated/compiled files required to run the tests", DefaultTask, target).dependsOn "cleanTestHtml", "cleanTestJs", "cleanTestLibs"
 
-        this.createTask("clean", DefaultTask, target).dependsOn "cleanTest", "cleanMain"
+        this.createTask("clean", "Delete all generated/compiled files", DefaultTask, target).dependsOn "cleanTest", "cleanMain"
 
-        this.createTask("tscCheck", TypeScriptCompilerCheckTask, target)
+        this.createTask("tscCheck", "Check the installed version of the TypeScript compiler", TypeScriptCompilerCheckTask, target)
 
-        this.createTask("copyVendorJs", CopyVendorJsTask, target)
-        this.createTask("copyRequirejsConfig", CopyRequirejsConfigTask, target)
-        this.createTask("combineGeneratedJs", CombineGeneratedJsTask, target).dependsOn "copyVendorJs", "copyRequirejsConfig", "compileTypeScript"
+        this.createTask("copyVendorJs", "Copy 3rd party libraries to the build directory", CopyVendorJsTask, target)
+        this.createTask("copyRequirejsConfig", "Copy RequireJS config to the build directory", CopyRequirejsConfigTask, target)
+        this.createTask("combineGeneratedJs", "Combine all JavaScript to a single file for better performance", CombineGeneratedJsTask, target).dependsOn "copyVendorJs", "copyRequirejsConfig", "compileTypeScript"
 
     }
 
-    private <T extends DefaultTask> T createTask(String name, Class<T> taskClass, Project project) {
+    private <T extends DefaultTask> T createTask(String name, String description, Class<T> taskClass, Project project) {
         T task = (T) project.task(name, type: taskClass)
+        task.description description
         if (task instanceof TypeScriptPluginTask) {
             task.setupInputsAndOutputs(TypeScriptPluginExtension.getInstance(project))
         }
