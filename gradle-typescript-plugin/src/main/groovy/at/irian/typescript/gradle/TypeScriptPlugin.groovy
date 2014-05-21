34a90be26c737727e058f5d6bd8cc5e2ec2b69f8
Jan Zarnikov <jan.zarnikov@irian.at>
2014-05-21 15:54:14 +0200

    fixed tasks naming conflicts with other plugins

--- gradle-typescript-plugin/src/main/groovy/at/irian/typescript/gradle/TypeScriptPlugin.groovy
+++ gradle-typescript-plugin/src/main/groovy/at/irian/typescript/gradle/TypeScriptPlugin.groovy
@@ -19,6 +19,7 @@ import at.irian.typescript.gradle.task.RunTestInBrowserTask
 import at.irian.typescript.gradle.task.RunTestInConsoleTask
 import at.irian.typescript.gradle.task.TypeScriptCompilerCheckTask
 import at.irian.typescript.gradle.task.TypeScriptPluginTask
+import org.gradle.api.Task
 
 class TypeScriptPlugin implements Plugin<Project> {
 
@@ -27,33 +28,32 @@ class TypeScriptPlugin implements Plugin<Project> {
         target.extensions.create("typescript", TypeScriptPluginExtension, target);
 
         this.createTask("compileTypeScript", "Compile TypeScript source code", CompileMainTask, target)
-        this.createTask("compileTestTypeScript", "Compile TypeScript test source code", CompileTestTask, target)
+        this.createTask("compileTypeScriptTest", "Compile TypeScript test source code", CompileTestTask, target)
 
 
-        this.createTask("initTestResources", "Initialize test resources (currently just jasmine.d.ts)", InitTestResourcesTask, target)
-        this.createTask("prepareTestLibs", "Prepare libraries requried to run the tests", PrepareTestLibsTask, target)
-        this.createTask("generateTestHtml", "Generate HTML required to run the tests", GenerateTestHtmlTask, target)
+        this.createTask("initTypeScriptTestResources", "Initialize test resources (currently just jasmine.d.ts)", InitTestResourcesTask, target)
+        this.createTask("prepareTypeScriptTestLibs", "Prepare libraries requried to run the tests", PrepareTestLibsTask, target)
+        this.createTask("generateTypeScriptTestHtml", "Generate HTML required to run the tests", GenerateTestHtmlTask, target)
 
-        this.createTask("test", "Run the tests in console mode", RunTestInConsoleTask, target).dependsOn "generateTestHtml", "prepareTestLibs", "compileTestTypeScript"
+        this.createTopLevelTask("test", "Run the tests in console mode", RunTestInConsoleTask, target).dependsOn "generateTypeScriptTestHtml", "prepareTypeScriptTestLibs", "compileTypeScriptTest"
 
-        this.createTask("testInBrowser", "Run the tests in browser", RunTestInBrowserTask, target).dependsOn "generateTestHtml", "prepareTestLibs", "compileTestTypeScript"
+        this.createTask("runTypeScriptTestsInBrowser", "Run the tests in browser", RunTestInBrowserTask, target).dependsOn "generateTypeScriptTestHtml", "prepareTypeScriptTestLibs", "compileTypeScriptTest"
 
-        this.createTask("cleanMain", "Delete compiled JavaScript", CleanMainTask, target)
+        this.createTask("cleanTypeScriptMain", "Delete compiled JavaScript", CleanMainTask, target)
 
-        this.createTask("cleanTestHtml", "Delete generated HTML required to run the tests", CleanTestHtmlTask, target)
-        this.createTask("cleanTestJs", "Delete compile test JavaScript", CleanTestJsTask, target)
-        this.createTask("cleanTestLibs", "Delete libraries requried to run the tests", CleanTestLibsTask, target)
+        this.createTask("cleanTypeScriptTestHtml", "Delete generated HTML required to run the tests", CleanTestHtmlTask, target)
+        this.createTask("cleanTypeScriptTestJs", "Delete compile test JavaScript", CleanTestJsTask, target)
+        this.createTask("cleanTypeScriptTestLibs", "Delete libraries requried to run the tests", CleanTestLibsTask, target)
 
-        this.createTask("cleanTest", "Delete all generated/compiled files required to run the tests", DefaultTask, target).dependsOn "cleanTestHtml", "cleanTestJs", "cleanTestLibs"
+        this.createTask("cleanTypeScriptTest", "Delete all generated/compiled files required to run the tests", DefaultTask, target).dependsOn "cleanTypeScriptTestHtml", "cleanTypeScriptTestJs", "cleanTypeScriptTestLibs"
 
-        this.createTask("clean", "Delete all generated/compiled files", DefaultTask, target).dependsOn "cleanTest", "cleanMain"
+        this.createTopLevelTask("clean", "Delete all generated/compiled files", DefaultTask, target).dependsOn "cleanTypeScriptTest", "cleanTypeScriptMain"
 
-        this.createTask("tscCheck", "Check the installed version of the TypeScript compiler", TypeScriptCompilerCheckTask, target)
+        this.createTopLevelTask("tscCheck", "Check the installed version of the TypeScript compiler", TypeScriptCompilerCheckTask, target)
 
-        this.createTask("copyVendorJs", "Copy 3rd party libraries to the build directory", CopyVendorJsTask, target)
-        this.createTask("copyRequirejsConfig", "Copy RequireJS config to the build directory", CopyRequirejsConfigTask, target)
-        this.createTask("combineGeneratedJs", "Combine all JavaScript to a single file for better performance", CombineGeneratedJsTask, target).dependsOn "copyVendorJs", "copyRequirejsConfig", "compileTypeScript"
-
+        this.createTask("copyTypeScriptVendorJs", "Copy 3rd party libraries to the build directory", CopyVendorJsTask, target)
+        this.createTask("copyTypeScriptRequirejsConfig", "Copy RequireJS config to the build directory", CopyRequirejsConfigTask, target)
+        this.createTask("combineTypeScriptGeneratedJs", "Combine all JavaScript to a single file for better performance", CombineGeneratedJsTask, target).dependsOn "copyTypeScriptVendorJs", "copyTypeScriptRequirejsConfig", "compileTypeScript"
     }
 
     private <T extends DefaultTask> T createTask(String name, String description, Class<T> taskClass, Project project) {
@@ -63,5 +63,21 @@ class TypeScriptPlugin implements Plugin<Project> {
             task.setupInputsAndOutputs(TypeScriptPluginExtension.getInstance(project))
         }
         return task
+    }
+    
+    private <T extends DefaultTask> T createTopLevelTask(String name, String description, Class<T> taskClass, Project project) {
+        Task existingTask = project.tasks.findByName(name);
+        if (existingTask != null) {
+            name = getPrefixedTaskName(name);
+        }
+        T task = this.createTask(name, description, taskClass, project);
+        if (existingTask != null) {
+            existingTask.dependsOn(task);
+        }
+        return task;
+    }
+
+    private String getPrefixedTaskName(String name) {
+        return "typeScript" + name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 }
